b389caf9398be16701799b02b280786368db5deb
nestorjhernandezm <nestor@steinwurf.com>
2017-04-23 03:33:02 +0200

    Working now. Problem was that the file were compiled each separately and not both together

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@ using namespace std;
 
 int main()
 {
+    cout << "Card to be created" << endl;
     Card a_card(Rank::Seven, Suit::Diamonds);
+    cout << "Card created" << endl;
 
     return 0;
 }
