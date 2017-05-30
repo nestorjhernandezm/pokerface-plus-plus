469b51658e26d40c66f3142670b2294dfaf90d92
nestorjhernandezm <nestor@steinwurf.com>
2017-05-30 21:58:39 +0200

    Adding tests for the card operators

--- test/src/test_card.cpp
+++ test/src/test_card.cpp
@@ -15,3 +15,14 @@ TEST(CardTest, CreateCard)
     EXPECT_EQ(Rank::Ace, card.rank());
     EXPECT_EQ(Suit::Spades, card.suit());
 }
+
+TEST(CardTest, CheckCardOperators)
+{
+    Card ace_of_diamonds(Rank::Ace, Suit::Diamonds);
+    Card ace_of_clubs(Rank::Ace, Suit::Clubs);
+    Card king_of_diamonds(Rank::King, Suit::Diamonds);
+
+    EXPECT_EQ(ace_of_diamonds == ace_of_diamonds, true);
+    EXPECT_EQ(ace_of_diamonds != ace_of_clubs, true);
+    EXPECT_EQ(ace_of_diamonds != king_of_diamonds, true);
+}
